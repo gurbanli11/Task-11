e8b7764d266d704bef73c98eb12f14bd1863141d
gurbanli11 <orkhan.80@bk.ru>
2024-04-03 02:59:02 +0400

    create encapsulation

---
+++ EncapsulationTask/EncapsulationTask/Models/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace EncapsulationTask.Models
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon();
+            Console.WriteLine(weapon.BulletCapacity);
+            weapon.Fire();
+            weapon.Shoot();
+            int num;
+            do
+            {
+                Console.WriteLine(@"0 - İnformasiya almaq üçün
+1 - Shoot metodu üçün
+2 - Fire metodu üçün
+3 -  GetRemainBulletCount metodu üçün
+4 - Reload metodu üçün
+5 - ChangeFireMode metodu üçün
+6 - Proqramdan dayandırmaq üçün
+qısayoldur.
+7 - Redaktə et :
+T - Tutumu dəyişsin
+S - Güllə sayı
+D - Sıfırlama saniyəsi");
+                if (int.TryParse(Console.ReadLine() ,out num)
+                {
+                    switch (num)
+                    {
+                        case 0:
+                            weapon.GetInfo();
+                            break;
+                        case 1:
+                            weapon.Shoot();
+                            break;
+                        case 2:
+                            weapon.Fire();
+                            break;
+                        case 3:
+                            weapon.GetRemainBulletCount();
+                            break;
+                        case 5:
+                            weapon.ChangeFireMode();
+                            break;
+                        default:
+                            Console.WriteLine()
+
+                    }
+                }
+
+            } while ();
+        }
+    }
+}
---
+++ EncapsulationTask/EncapsulationTask/Models/Weapon.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationTask.Models
+{
+    internal class Weapon
+    {
+        public int BulletCapacity;
+        public int BulletNum;
+        public int FinishSecond;
+        public bool SingleShootingMode;
+        public bool AutomaticShootingMode;
+
+
+        public Weapon(int bulletCapacity, int bulletNum, int finishSecond, bool singleShootingMode, bool automaticShootingMode)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletNum = bulletNum;
+            FinishSecond = finishSecond;
+            SingleShootingMode = singleShootingMode;
+            AutomaticShootingMode = automaticShootingMode;
+        }
+    
+        public void Shoot()
+        {
+            if (SingleShootingMode)
+            {
+                if (BulletNum==0)
+                {
+                    Console.WriteLine("Daraqda gulle yoxdur");
+                }
+                BulletNum--;
+                Console.WriteLine("1 gulle atildi");
+            }
+        }
+        public void Fire()
+        {
+            while (BulletNum>0)
+            {
+                BulletNum--;
+            }
+        }
+        public void GetRemainBulletCount()
+        {
+            Console.WriteLine(BulletCapacity-BulletNum);
+        }
+        public void Reload()
+        {
+            while (BulletNum==BulletCapacity)
+            {
+                BulletNum++;
+            }
+        }
+        public void ChangeFireMode()
+        {
+
+        }
+    }
+}
